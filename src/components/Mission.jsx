900bfc0a827dddc731e9ff07a6e48d9340e1946c
Zachary Hiner <zachhiner@Hiner-4.local>
2025-03-28 15:16:41 -0400

    mission.jsx fake bottle section mobile responsive

--- src/components/Mission.jsx
+++ src/components/Mission.jsx
@@ -3,13 +3,13 @@ import { products } from "../constants"; // Import the product data
 
 const Mission = () => {
   return (
-    <div id="mission" className="h-[90vh] bg-bone overflow-hidden">
+    <div id="mission" className="h-[180vh] md:h-[90vh] bg-bone overflow-hidden">
       {/* Header outside the flex container */}
-      <h1 className=" text-4xl text-center mt-20 my-20 tracking-wide font-breathdemo text-roseda ">
+      <h1 className="text-2xl mt-10 mx-4 md:text-4xl text-center md:mt-20 md:my-20 tracking-wide font-breathdemo text-roseda ">
         Refreshing Hydration for Adventures
       </h1>
       {/* Flex container for products */}
-      <div className="flex flex-wrap justify-center gap-10 md:gap-10 lg:gap-20 mt-8 ">
+      <div className="flex flex-col md:flex-row justify-center items-center gap-10 md:gap-20 mt-8 ">
         {products.map((product, index) => (
           <div
             key={index}
@@ -18,7 +18,7 @@ const Mission = () => {
             <img
               src={product.image}
               alt={`${product.bottleColor} Bottle`}
-              className="object-cover rounded-xl h-20 md:h-44 lg:h-60 xl:h-96"
+              className="flex justify-center object-cover rounded-xl h-80 lg:h-96"
             />
             <h2 className="mt-4 text-xl font-bold text-white font-oxygenlight">
               {`${product.bottleColor} Bottle`}
